19fe2669ba89ed1b4d94951a8e28156ff89f0b8d
David Arriaga <alimentadorweb@websupportels.com>
2018-04-17 09:19:59 -0600

    Version 0.1
    Contador de visitas configurable

---
+++ conteo.php
@@ -0,0 +1,16 @@
+<?php
+$archivo = "contador.txt";
+$contador = 0;
+
+$fp = fopen($archivo,"r");
+$contador = fgets($fp, 26);
+fclose($fp);
+
+++$contador;
+
+$fp = fopen($archivo,"w+");
+fwrite($fp, $contador, 26);
+fclose($fp);
+
+echo "Esta página fue visitada $contador veces";
+?> 
